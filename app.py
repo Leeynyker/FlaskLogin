1bfcb5c3bee9a78f7b5c2b820ed59acdc95452b7
Leey <leeynykeramr@gmail.com>
2021-03-10 01:01:08 -0500

    Validación de campos con JS

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_wtf import CsrfProtect
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from flask_cors import CORS
+import bcrypt
 
 app = Flask(__name__)
+app.secret_key = '12345'
+csrf = CsrfProtect
+# CORS = app
 
+def conexion():
+   return psycopg2.connect(
+       host="localhost",
+      #  database="loginDB",
+       database="practica1",
+       user = "postgres",
+       password="andres601"
+      )
+   
 @app.route('/')
 def index():
+   # conn = conexion()
+   # cur = conn.cursor(cursor_factory=RealDictCursor)
+   # cur.execute("SELECT * FROM usuarios")
+   # rows = cur.fetchall() 
+   # conn.close()
+   # cur.close()
+   # return render_template('index.html', rowse = rows)
    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   if request.method == 'POST':
+      username = request.form['username']
+      email = request.form['email']
+      password = request.form['pw']
+      checkpassword = request.form['check-pw']
+      
+           
+      # print(username, email, password)
+      return render_template('succes.html')
+   
+# @app.route("/insert")
+# def insert_default():
+#    conn = conexion()
+#    cur = conn.cursor(cursor_factory=RealDictCursor)
+#    cur.execute('w)
+#    rows = cur.fetchall() 
+#    conn.close()
+#    cur.close()
+#    return "The  default post was created"
 
 if __name__=="__main__":
   app.run(debug=True, port=5000, host='0.0.0.0')
--- static/js/app.js
+++ static/js/app.js
@@ -9,3 +9,38 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+function habilitar() {
+  pw = document.getElementById("pw");
+  ckpw = document.getElementById("check-pw");
+  username = document.getElementById("username");
+  email = document.getElementById("email");
+  boton = document.getElementById("btnReg");
+
+  let flag = true;
+  // Validar campos vacios
+  if (pw.value.length == 0 || username.value.length == 0 || email.value.length == 0 ) {
+    document.getElementById("pTipd").innerHTML = "*     Debe llenar todos los campos";
+    flag = true;
+  } else {
+    document.getElementById("pTipd").innerHTML = "";
+    flag = false;
+  }
+
+
+  // Validar contraseñas
+  if (pw.value != ckpw.value ) {
+    document.getElementById("pPass").innerHTML = "*     Las contraseñas no coniciden";
+    flag = true;
+  } else{
+    if (pw.value.length == 0) {
+      flag = true;
+    } else {
+      document.getElementById("pPass").innerHTML = "";
+      flag = false;
+    }
+    document.getElementById("pPass").innerHTML = "";
+  }
+
+  boton.disabled = flag;
+}
